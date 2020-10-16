412562f24618f1cb4d82a6063defc54b78fbd766
tediko <tediko123@wp.pl>
2020-10-16 12:04:05 +0200

    Added hamburger menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log('hello!');
+/* HAMBURGER MENU BUTTON */
+const menuBtn = document.querySelector('.nav__mobile-btn');
+let isMenuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+    if(!isMenuOpen) {
+        menuBtn.classList.add('open');
+        isMenuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        isMenuOpen = false;
+    }
+});
