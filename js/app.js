5d91f761e60abc4fe7cee648cb0acd935f75a108
tediko <tediko123@wp.pl>
2020-10-16 12:24:11 +0200

    Added mobile menu

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 /* HAMBURGER MENU BUTTON */
+const menu = document.querySelector('.nav__menu');
 const menuBtn = document.querySelector('.nav__mobile-btn');
 let isMenuOpen = false;
 
 menuBtn.addEventListener('click', () => {
     if(!isMenuOpen) {
+        menu.classList.add('open');
         menuBtn.classList.add('open');
         isMenuOpen = true;
     } else {
+        menu.classList.remove('open');
         menuBtn.classList.remove('open');
         isMenuOpen = false;
     }
