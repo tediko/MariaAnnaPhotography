a9f8b6cca9da5ca7875f32761621a69178391c15
tediko <tediko123@wp.pl>
2020-10-18 13:11:58 +0200

    Added slider functionality

--- js/app.js
+++ js/app.js
@@ -14,3 +14,31 @@ menuBtn.addEventListener('click', () => {
         isMenuOpen = false;
     }
 });
+
+/* TESTIMONIAL SLIDER */
+const slides = document.querySelectorAll('.testimonial__slide');
+const track = document.querySelector('.testimonial__sliderTrack');
+const indicators = document.querySelectorAll('.testimonial__indicator');
+
+slides.forEach((slide, idx) => { // set each slide position
+    slide.style = `left: ${800 * idx}px`;
+})
+
+function slide(index, indicator) { //change slide translating X position
+    track.style.transform = `translateX(-${index * 800}px)`;
+    removeActive(); 
+    indicator.classList.add('active'); //add active class to clicked indicator button
+}
+
+function removeActive() { // remove all active class from indicator buttons
+    indicators.forEach(indicator => {
+        indicator.classList.remove('active');
+    })
+}
+
+indicators.forEach((indicator, idx) => {
+    indicator.addEventListener('click', () => {
+        slide(idx, indicator);
+    })
+})
+
